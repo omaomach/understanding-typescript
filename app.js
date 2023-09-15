829db86d4e04405c4bbdad937ce71f718a16f821
omaomach <aaronjoash44@gmail.com>
2023-09-15 20:37:47 +0300

    worked on typescript type casting

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Stay focused please!!");
+function add(n1, n2) {
+    return n1 + n2;
+}
+var num1 = "5";
+var num2 = 5;
+var result = add(+num1, +num2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,9 @@
-console.log("Stay focused please!!")
+function add(n1: number, n2: number) {
+    return n1 + n2
+}
+
+const num1 = 5
+const num2 = 5
+
+const result = add(num1, num2)
+console.log(result)
