941399298326ead3d1713529fca6f63ffc9803d5
omaomach <aaronjoash44@gmail.com>
2024-01-30 20:57:43 +0300

    worked on Type Assignment and Type Inference

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, showResult) {
+    // if (typeof n1 !== "number" || typeof n2 !== "number") {
+    //     throw new Error("Input not a number")
+    // }
+    if (showResult) {
+        console.log(n1 + n2);
+    }
+    else {
+        return n1 + n2;
+    }
 }
-var num1 = "5";
-var num2 = 5;
-var result = add(+num1, +num2);
-console.log(result);
+var num1 = 25;
+var num2 = 10;
+var printResult = true;
+var result = add(num1, num2, printResult);
+console.log("This is the result ".concat(result));
--- app.ts
+++ app.ts
@@ -1,9 +1,17 @@
-function add(n1: number, n2: number) {
-    return n1 + n2
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+    // if (typeof n1 !== "number" || typeof n2 !== "number") {
+    //     throw new Error("Input not a number")
+    // }
+    if (showResult) {
+        console.log(n1 + n2)
+    } else {
+        return n1 + n2
+    }
 }
 
-const num1 = 5
-const num2 = 5
+const num1 = 25
+const num2 = 10
+const printResult = true;
 
-const result = add(num1, num2)
-console.log(result)
+const result = add(num1, num2, printResult)
+console.log(`This is the result ${result}`)
