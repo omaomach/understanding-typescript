bbe56b9a2706c6aed74ffb05c11b8a297414abe7
omaomach <aaronjoash44@gmail.com>
2024-02-09 18:09:27 +0300

    worked on type aliases and object types

---
+++ type-aliases-and-object-types.js
@@ -0,0 +1,17 @@
+var u1 = { name: "Joel", age: 22 };
+// We can simplify this code
+// function greet(user: { name: string; age: number }) {
+//   console.log("Hi, I am " + user.name);
+// }
+// function isOlder(user: { name: string; age: number }, checkAge: number) {
+//   return checkAge > user.age;
+// }
+// To
+function greet(user) {
+    console.log("Hi " + user.name);
+}
+function isOlder(user) {
+    return user.age > 24;
+}
+greet({ name: "Joash", age: 25 });
+console.log(isOlder({ name: "Joash", age: 25 }));
