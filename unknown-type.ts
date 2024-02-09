6c4e9bc9aca8d64e879d77b25beb4ed4e81deca2
omaomach <aaronjoash44@gmail.com>
2024-02-09 21:15:52 +0300

    working on unknown types

---
+++ unknown-type.ts
@@ -0,0 +1,11 @@
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Joshua"
+
+// userName = userInput;
+
+if (typeof userInput === "string") {
+    userName = userInput;
+}
