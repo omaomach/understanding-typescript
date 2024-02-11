6a911eedc5267521f0c30423065074089e3f672a
omaomach <aaronjoash44@gmail.com>
2024-02-12 02:10:45 +0300

    working on unknown types

--- unknown-type.js
+++ unknown-type.js
@@ -1,12 +1,9 @@
-let userInput: unknown; // a bit more ristictive as compared to the any type
+var userInput; // a bit more ristictive as compared to the any type
 // let userInput: any; // here typescript does not check for any types at all.
-let userName: string;
-
+var userName;
 userInput = 5;
-userInput = "Joshua"
-
+userInput = "Joshua";
 // userName = userInput;
-
 if (typeof userInput === "string") {
     userName = userInput;
 }
